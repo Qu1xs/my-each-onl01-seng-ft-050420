6b75aaed19c56489be96a6a40f653bc001b5dced
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 15:27:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,24 +2,11 @@ def my_each(array)
   i = 0
     while i < array.length
       yield array[i]
-      i = i + 1
+      i += 1
     end
 end
 
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
-end
-
-def hello_t(array)
-  if block_given?
-    i = 0
-    while i < array.length
-      yield array[i]
-      i = i + 1
-    end
-    array
-  else
-     puts "Hey! No block was given!"
-  end 
 end
